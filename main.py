45e4e96c76c298d42e80bbf02d0508d435359675
Ganesh Kadu <64672771+ganeshkadu1@users.noreply.github.com>
2022-09-18 16:23:55 +0530

    Refactor a logic

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ while not SHUT_DOWN:
     else:
         menu_item = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(menu_item):
-            money_machine.make_payment(menu_item.cost)
-            coffee_maker.make_coffee(menu_item)
+           if money_machine.make_payment(menu_item.cost):
+                coffee_maker.make_coffee(menu_item)
 
 
 
